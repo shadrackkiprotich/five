86ed959838921b642f42684d5b0b9f33fe37d0fe
shadrackkiprotich <mylesshadrack99@gmail.com>
2019-02-08 23:38:20 +0300

    five

---
+++ five/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace five
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter two numbers");
+            int maxnumber = Convert.ToInt32(Console.ReadLine());
+            int minnumber = Convert.ToInt32(Console.ReadLine());
+            for (int k = maxnumber; k > minnumber; k--)
+            {
+                if ((k % 10) == 5)
+                {
+                    int[] five = { k };
+                    Console.WriteLine(k);
+                    // Console.WriteLine(k);
+
+                    // k++;
+
+                    count++;
+                }
+
+
+            }
+            Console.WriteLine("{0}",count);
+            Console.ReadKey();
+        }
+    }
+}
